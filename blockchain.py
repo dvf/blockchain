5f25d92be6857b2a17566bf66ac40422d19f19de
mbeigel <michael.beigelmacher@gmail.com>
2017-09-30 18:07:06 -0400

    Add option to choose port from command line

--- blockchain.py
+++ blockchain.py
@@ -279,4 +279,11 @@ def consensus():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
