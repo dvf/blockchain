d1afbe5e50ed55f92168befa3d0f4b7e4d8af076
blueplanet <erguolinge@gmail.com>
2017-10-14 11:19:58 +0900

    Add blockchain class

---
+++ ruby/blockchain.rb
@@ -0,0 +1,21 @@
+class Blockchain
+  def initialize
+    @chain = []
+    @current_transactions = []
+  end
+
+  def new_block
+  end
+
+  def new_transaction
+  end
+
+  def self.hash
+    
+  end
+
+  protected
+
+  def last_block
+  end
+end
