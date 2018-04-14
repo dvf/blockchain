5c7c1d25dea71faa6deb943eac7e96281cc2f912
masayuki tanaka <masayuki5160@gmail.com>
2018-04-15 08:52:46 +0900

    Fix Block class properties

--- src/main/kotlin/Block.kt
+++ src/main/kotlin/Block.kt
@@ -1,7 +1,7 @@
 data class Block(
         val index: Int,
-        val timestamp: Int,
-        val transactions: List<Block>,
+        val timestamp: Double,
+        val transactions: List<Transaction>,
         val proof: Int,
         val previousHash: String
 )
