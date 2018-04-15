eb02afac77ce0af2314b2b64b03cf9e05e543706
masayuki tanaka <masayuki5160@gmail.com>
2018-04-15 19:13:07 +0900

    Add proofOfWork() and tests for proofOfWork()

--- src/main/kotlin/Blockchain.kt
+++ src/main/kotlin/Blockchain.kt
@@ -35,4 +35,27 @@ object Blockchain {
         return digest.fold("", { str, it -> str + "%02x".format(it) })// TODO 各メソッドの仕様を再確認したい
     }
 
+    // hash(前のproof + proof)の最初の4文字が0となるhash値を探す
+    fun proofOfWork(lastProof: String): String {
+        var proof = 0
+        while (!validWork(lastProof, Integer.toString(proof))) {
+            proof += 1
+        }
+        return Integer.toString(proof)
+    }
+
+    fun validWork(lastProof: String, proof: String): Boolean {
+        val digest = MessageDigest.getInstance("SHA-256").digest((lastProof + proof).toByteArray())
+        val hashVal = digest.fold("", {str, it -> str + "%02x".format(it)})
+
+        return hashVal.substring(0, 4) == "0000"
+    }
+
+    fun convertHash(string: String): String {
+        val digest = MessageDigest.getInstance("SHA-256").digest(string.toByteArray())
+        val hashVal = digest.fold("", {str, it -> str + "%02x".format(it)})
+
+        return hashVal
+    }
+
 }
--- src/test/kotlin/TestBlockchain.kt
+++ src/test/kotlin/TestBlockchain.kt
@@ -1,34 +1,46 @@
 import org.junit.Test
-import org.junit.Assert.assertThat
 import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 
 class TestBlockchain {
 
     // TODO 不要かな？
     @Test fun hash値の一貫性を確認する() {
-        var blockchain = Blockchain
+        val blockchain = Blockchain
 
         // blockの一つ目を用意する
-        var transaction01: Transaction = Transaction("1", "2", 1)
-        var transaction02: Transaction = Transaction("2", "3", 5)
+        val transaction01: Transaction = Transaction("1", "2", 1)
+        val transaction02: Transaction = Transaction("2", "3", 5)
 
-        var transactionsA: MutableList<Transaction>? = null
+        val transactionsA: MutableList<Transaction>? = null
         transactionsA?.add(transaction01)
         transactionsA?.add(transaction02)
 
-        var blockA = Block(1, 1, transactionsA, 1, "hash")
-        var hashA: String = blockchain.hash(blockA)
+        val blockA = Block(1, 1, transactionsA, 1, "hash")
+        val hashA: String = blockchain.hash(blockA)
 
         // 同じtransactionだが別のインスタンスでblockを用意する
-        var transactionsB: MutableList<Transaction>? = null
+        val transactionsB: MutableList<Transaction>? = null
         transactionsB?.add(transaction01)
         transactionsB?.add(transaction02)
 
-        var blockB = Block(1, 1, transactionsB, 1, "hash")
-        var hashB: String = blockchain.hash(blockB)
+        val blockB = Block(1, 1, transactionsB, 1, "hash")
+        val hashB: String = blockchain.hash(blockB)
 
         // 同じtransactionが入ったblockは同じhash値になることを確認する
         assertEquals(hashA, hashB)
+    }
+
+    @Test fun proofOfWorkで発見したhash値の先頭4文字が0になっていること() {
+        val blockchain = Blockchain
+        val lastProof = "10"
+
+        val proof = blockchain.proofOfWork(lastProof)
+        println("proof：" + proof)
+
+        val hashVal = blockchain.convertHash(lastProof + proof)
+        println("hash：" + hashVal)
+
+        assertEquals("0000", hashVal.substring(0, 4))
     }
 }
