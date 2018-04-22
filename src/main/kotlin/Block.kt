9f2ceb365b4d0815ea1994ed4dc7a0df1533369d
masayuki tanaka <masayuki5160@gmail.com>
2018-04-22 10:13:00 +0900

    Remove unused dataclass

--- src/main/kotlin/Block.kt
+++
@@ -1,7 +0,0 @@
-data class Block(
-        val index: Int,
-        val timestamp: Long,
-        val transactions: MutableList<Transaction>?,
-        val proof: Int,
-        val previousHash: String
-)
