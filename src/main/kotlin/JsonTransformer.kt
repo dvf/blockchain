6e75e3607a09e4a1a1566647928b42f13f01a86f
masayuki tanaka <masayuki5160@gmail.com>
2018-04-22 09:45:27 +0900

    Use path()

---
+++ src/main/kotlin/JsonTransformer.kt
@@ -0,0 +1,6 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import spark.ResponseTransformer
+
+class JsonTransformer(private val objectMapper: ObjectMapper): ResponseTransformer {
+    override fun render(model: Any?): String = objectMapper.writeValueAsString(model)
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,42 @@
 import spark.Spark.get
+import spark.Spark.post
+import spark.Spark.delete
+import spark.Spark.path
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 fun main(args: Array<String>){
 
     val objectMapper = ObjectMapper().registerKotlinModule()
-    val blockChain: Blockchain = Blockchain()
+    val jsonTransformer = JsonTransformer(objectMapper)
+    val blockChain = Blockchain()
 
-    // 新しいBlockを採掘する
-    get("/mine") { req, res ->
-        "新しいBlockを採掘する"
+    path("/transactions") {
+        get("/new") { req, res ->
+            "新しいBlockを採掘する"
+        }
     }
 
-    // フルのブロックチェーンをリターンする
-    get("/chain") { req, res ->
-        objectMapper.writeValueAsString(blockChain.chain)
+    path("/mine") {
+        // 新しいBlockを採掘する
+        get("") { req, res ->
+            "新しいBlockを採掘する"
+        }
+    }
+
+    path("/chain") {
+        // フルのブロックチェーンをリターンする
+        get("") { req, res ->
+            objectMapper.writeValueAsString(blockChain.chain)
+        }
+    }
+
+    path("/nodes") {
+        get("/register") { req, res ->
+            "新しいnodeを登録する"
+        }
+        get("/resolve") { req, res ->
+            "チェーンが確認されました"
+        }
     }
 }
