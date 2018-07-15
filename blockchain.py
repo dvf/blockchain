4b96238f510f3165594713a4c438f81d728e144e
Edward <edwardgonsalves24@gmail.com>
2018-07-15 16:11:36 +0530

    Handling nonresponsive nodes

--- blockchain.py
+++ blockchain.py
@@ -80,16 +80,19 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            try:
+                response = requests.get(f'http://{node}/chain')
+                if response.status_code == 200:
+                    length = response.json()['length']
+                    chain = response.json()['chain']
 
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+                    # Check if the length is longer and the chain is valid
+                    if length > max_length and self.valid_chain(chain):
+                        max_length = length
+                        new_chain = chain
+            except requests.exceptions.ConnectionError:
+                print(f'Node: {node} not responding')
+                pass
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if new_chain:
